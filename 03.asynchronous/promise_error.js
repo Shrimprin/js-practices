2da85348b0819e6429eb43fd3290b8a20134d57f
Kuroneko <ken.aster8@gmail.com>
2024-01-14 17:08:41 +0900

    promiseのエラーありを作成

---
+++ 03.asynchronous/promise_error.js
@@ -0,0 +1,24 @@
+import { newDb, run, all } from "./sqlite_utils.js";
+
+let db;
+newDb(":memory:")
+  .then((result) => {
+    db = result;
+    return run(
+      db,
+      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(20) UNIQUE NOT NULL)",
+    );
+  })
+  .then(() => run(db, "INSERT INTO books (name) VALUES (?)", null))
+  .catch((error) => {
+    console.error(error.message);
+    return all(db, "SELECT * FROM nox_exist_table");
+  })
+  .then((result) => {
+    console.log(result);
+    run(db, "DROP TABLE books");
+  })
+  .catch((error) => {
+    console.error(error.message);
+    run(db, "DROP TABLE books");
+  });
