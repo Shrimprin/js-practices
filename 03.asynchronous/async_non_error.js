39ce93a63edf659b56baaea5bd2814d6bfce5517
Kuroneko <ken.aster8@gmail.com>
2024-01-14 19:37:19 +0900

    async/awaitのエラー無しを作成

---
+++ 03.asynchronous/async_non_error.js
@@ -0,0 +1,22 @@
+import { newDb, run, all } from "./sqlite_utils.js";
+
+const db = await newDb(":memory:");
+
+await run(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(20) UNIQUE NOT NULL)",
+);
+
+const titles = ["JavaScript本格入門", "JavaScriptひらがなプログラミング"];
+for (const title of titles) {
+  // forEach(async () => {}で書くとトップレベルの次の処理が実行されるため、for ofで記述
+  const result = await run(db, "INSERT INTO books (title) VALUES (?)", title);
+  console.log(`ID:${result.lastID}`);
+}
+
+const books = await all(db, "SELECT * FROM books");
+books.forEach((book) => {
+  console.log(book);
+});
+
+run(db, "DROP TABLE books");
