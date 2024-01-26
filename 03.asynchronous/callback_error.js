f8381f9231e58488bc02d3e00199bf0fd19579f6
Kuroneko <ken.aster8@gmail.com>
2024-01-26 14:53:52 +0900

    ifの条件を集約

--- 03.asynchronous/callback_error.js
+++ 03.asynchronous/callback_error.js
@@ -5,18 +5,22 @@ const db = new sqlite3.Database(":memory:", () => {
     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT UNIQUE NOT NULL)",
     () => {
       db.run("INSERT INTO books (title) VALUES (?)", null, function (error) {
-        if (error) {
-          if (error instanceof Error && error.code === "SQLITE_CONSTRAINT") {
-            console.error(error.message);
-          }
+        if (
+          error &&
+          error instanceof Error &&
+          error.code === "SQLITE_CONSTRAINT"
+        ) {
+          console.error(error.message);
         } else {
           console.log(`ID:${this.lastID}`);
         }
         db.all("SELECT * FROM nox_exist_table", (error, books) => {
-          if (error) {
-            if (error instanceof Error && error.code === "SQLITE_ERROR") {
-              console.error(error.message);
-            }
+          if (
+            error &&
+            error instanceof Error &&
+            error.code === "SQLITE_ERROR"
+          ) {
+            console.error(error.message);
           } else {
             books.forEach((book) => {
               console.log(book);
