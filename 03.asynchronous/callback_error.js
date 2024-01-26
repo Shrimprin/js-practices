2076374581e50c1fc2b470b9f9e18efa7f754fc5
Kuroneko <ken.aster8@gmail.com>
2024-01-26 14:49:14 +0900

    INSERTとSELECTの処理を対象になるように修正

--- 03.asynchronous/callback_error.js
+++ 03.asynchronous/callback_error.js
@@ -4,13 +4,15 @@ const db = new sqlite3.Database(":memory:", () => {
   db.run(
     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT UNIQUE NOT NULL)",
     () => {
-      db.run("INSERT INTO books (title) VALUES (?)", null, (error) => {
+      db.run("INSERT INTO books (title) VALUES (?)", null, function (error) {
         if (error) {
           if (error instanceof Error && error.code === "SQLITE_CONSTRAINT") {
             console.error(error.message);
           }
+        } else {
+          console.log(`ID:${this.lastID}`);
         }
-        db.get("SELECT * FROM non_exist_table", (error, books) => {
+        db.all("SELECT * FROM nox_exist_table", (error, books) => {
           if (error) {
             if (error instanceof Error && error.code === "SQLITE_ERROR") {
               console.error(error.message);
